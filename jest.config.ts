5a91eef62a9347de8672f5f46e5bbcf432616e2e
Ethan Donahue <donahue.ethanj@gmail.com>
2025-02-11 23:09:31 -0500

    User api (#1)

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',         
+  testEnvironment: 'node',   
+  transform: {
+    '^.+\\.ts$': 'ts-jest', 
+  },
+  moduleFileExtensions: ['ts', 'js'], 
+  testPathIgnorePatterns: ['/node_modules/'],  
+};
+
+export default config;
