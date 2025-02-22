fa77cd032a698b92355cc2f2ffc81d5baab4065c
Rachel La <89667709+rachella0418@users.noreply.github.com>
2025-02-22 11:44:14 -0500

    added more friend queries

--- src/routes/friends/friends.routes.ts
+++ src/routes/friends/friends.routes.ts
@@ -44,7 +44,7 @@ router.post("/send", async (req: any, res: any) => {
 
 // Accept a friend request
 router.post("/accept", async (req: any, res: any) => {
-  const { request_id } = req.body;
+  const { request_id, user_id, friend_id } = req.body;
 
   if (!request_id) {
     return res.status(400).send("request_id is required");
@@ -52,16 +52,27 @@ router.post("/accept", async (req: any, res: any) => {
 
   try {
     const query =
-      "UPDATE friend_requests SET ispending = FALSE WHERE request_id = $1 AND ispending = TRUE RETURNING *";
+      "DELETE FROM friend_requests WHERE request_id = $1 RETURNING *";
     const values = [request_id];
 
     const result = await client.query(query, values);
 
     if (result.rowCount && result.rowCount > 0) {
-      res.status(200).json({ message: "Friend request accepted." });
+      const addFriendQuery =
+      "INSERT INTO friends (userid, friendid) VALUES ($1, $2) RETURNING *";
+      
+      const addFriendValues = [user_id, friend_id];
+      const addFriendResult = await client.query(addFriendQuery, addFriendValues);
+      
+      if (addFriendResult.rowCount && addFriendResult.rowCount > 0) {
+        res.status(200).json({ message: "Friend request accepted." });
+      } else {
+        res.status(404).send("Friend request not found or already processed.");
+      }
     } else {
       res.status(404).send("Friend request not found or already processed.");
     }
+
   } catch (error) {
     console.error("Error accepting friend request:", error);
     res.status(500).send("Error accepting friend request");
@@ -120,6 +131,58 @@ router.get("/requests", async (req: any, res: any) => {
   }
 });
 
+// Get all pending friend requests for a user
+router.get("/requests/pending", async (req: any, res: any) => {
+  const { userid } = req.query;
+
+  if (!userid) {
+    return res.status(400).send("userid is required");
+  }
+
+  try {
+    const query =
+      "SELECT * FROM friend_requests WHERE friendid = $1";
+    const values = [userid];
+
+    const result = await client.query(query, values);
+
+    if (result.rows.length > 0) {
+      res.status(200).json(result.rows)
+    } else {
+      res.status(404).send("No friend requests found");
+    }
+  } catch (error) {
+    console.error("Error retrieving friend requests:", error);
+    res.status(500).send("Error retrieving friend requests");
+  }
+});
+
+// Get all friend requests sent from a user
+router.get("/requests/sent", async (req: any, res: any) => {
+  const { userid } = req.query;
+
+  if (!userid) {
+    return res.status(400).send("userid is required");
+  }
+
+  try {
+    const query =
+      "SELECT * FROM friend_requests WHERE userid = $1";
+    const values = [userid];
+
+    const result = await client.query(query, values);
+
+    if (result.rows.length > 0) {
+      res.status(200).json(result.rows);
+    } else {
+      res.status(404).send("No friend requests found");
+    }
+  } catch (error) {
+    console.error("Error retrieving friend requests:", error);
+    res.status(500).send("Error retrieving friend requests");
+  }
+});
+
 // Delete a friend request
 router.delete("/request", async (req: any, res: any) => {
   const { request_id } = req.query;
@@ -145,5 +208,35 @@ router.delete("/request", async (req: any, res: any) => {
     res.status(500).send("Error deleting friend request");
   }
 });
+
+// Get all friend ids of a user by userid
+router.get("/", async (req: any, res: any) => {
+  const { userId } = req.query;
+
+  if (!userId) {
+    return res.status(400).send("userId is required");
+  }
+
+  if (isNaN(userId)) {
+    return res.status(400).send("Invalid userId");
+  }
+
+  try {
+    const query = "SELECT * FROM friends WHERE userid = $1";
+    const values = [userId];
+    const result = await client.query(query, values);
+
+    if (result.rows.length > 0) {
+      const friendIds = result.rows.map(row => row.friendid);
+      res.status(200).json(friendIds);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    console.error("Error retrieving user's friends:", error);
+    res.status(500).send("Error retrieving user's friends");
+  }
+
+})
 
 export default router;
